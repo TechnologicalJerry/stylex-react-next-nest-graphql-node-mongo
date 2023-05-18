716f0aa6882c53d46c9c73768bc4f4cd2d4e21d5
technologicaljerry <cserajatverma@gmail.com>
2023-05-18 23:54:11 +0530

    add book

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { BookModule } from './book/book.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, BookModule],
   controllers: [AppController],
   providers: [AppService],
 })
